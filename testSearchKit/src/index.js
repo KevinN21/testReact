f662fe61f6e826df5037eb79f02b4e6a798455aa
KevinN21 <kevin21.nguyen@gmail.com>
2017-03-16 15:47:12 +0100

    update searchkit

--- testSearchKit/src/index.js
+++ testSearchKit/src/index.js
@@ -1,26 +1,70 @@
-import * as React from 'react';
-import * as ReactDom from 'react-dom';
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import './index.css'
+
 import {
-  Hits,
-  InitialLoader,
-  HitItemProps,
-  SearchkitComponent
+    SearchkitManager,
+    SearchkitProvider,
+    Hits,
+    SearchBox,
+    Layout,
+    TopBar,
+    LayoutBody,
+    SideBar,
+    HierarchicalMenuFilter,
+    RefinementListFilter,
+    LayoutResults,
+    ActionBar,
+    ActionBarRow,
+    HitsStats,
+    SelectedFilters,
+    ResetFilters,
+    MovieHitsGridItem,
+    NoHits
 } from "searchkit";
 
-const InitialLoaderComponent = (props) => (
-  <div className={props.bemBlocks.item().mix(props.bemBlocks.container("item"))}>
-    loading please wait...
-  </div>
-)
+const searchkit = new SearchkitManager("http://demo.searchkit.co/api/movies/")
 
-class App extends SearchkitComponent {
+class App extends React.Component {
 
-  render(){
-    <div>
-      <Hits hitsPerPage={50} highlightFields={["title"]} sourceFilter={["title"]}/>
-      <InitialLoader component={InitialLoaderComponent}/>
-    </div>
-  }
-}
+    render() {
+        return (
 
-ReactDom.render(<App />, document.body)
+            <SearchkitProvider searchkit={searchkit}>
+                <Layout>
+                    <TopBar>
+
+                        <SearchBox autofocus={true} searchOnChange={true} prefixQueryFields={["actors^1", "type^2", "languages", "title^10"]} placeholder={'coucou'}/>
+                    </TopBar>
+
+                    <LayoutBody>
+                        <SideBar>
+                            <HierarchicalMenuFilter fields={["type.raw", "genres.raw"]} title="Categories" id="categories"/>
+                            <RefinementListFilter id="Ahbon" title="Ahbon" field="actors.raw" operator="aw" size={10}/>
+                        </SideBar>
+
+                        <LayoutResults>
+                            <ActionBar>
+
+                                <ActionBarRow>
+                                    <HitsStats/>
+                                </ActionBarRow>
+
+                                <ActionBarRow>
+                                    <SelectedFilters/>
+                                    <ResetFilters/>
+                                </ActionBarRow>
+
+                            </ActionBar>
+                            <Hits mod="sk-hits-grid" hitsPerPage={10} itemComponent={MovieHitsGridItem} sourceFilter={["title", "poster", "imdbId"]}/>
+                            <NoHits/>
+                        </LayoutResults>
+                    </LayoutBody>
+                </Layout>
+            </SearchkitProvider>
+        )
+    }
+};
+
+ReactDOM.render(
+    <App/>, document.body);
